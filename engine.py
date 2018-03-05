462b92f93bb815f376cd58d500870688cc6b8218
Suprabhat <suprabhatgurrala@gmail.com>
2018-03-05 16:13:46 -0500

    Reworked event object. Added GVT and peek method to FEL.

--- engine.py
+++ engine.py
@@ -1,28 +1,26 @@
-import time
-
-
 class Event:
     """
     Object to handle events.
     """
-    def __init__(self, event_data, event_handler):
+    def __init__(self, event_data, event_handler, event_type, name=""):
         """
         Initializes event object
         :param event_data: data specific to the event instance
         :param event_handler: a method to handle the event, takes event_data as a paramter
         """
-        self.timestamp = time.time()
-        self.event_data = event_data
-        self.event_handler = event_handler
+        self.data = event_data
+        self.handler = event_handler
+        self.type = event_type
+        self.name = name
 
     def handle(self):
         """
         calls the provided event handler with event_data as a parameter
         """
-        self.event_handler(self.event_data)
+        self.handler(self.data)
 
     def to_string(self):
-        return "Event object "#with data" + str(self.event_data)   + " at " + time.strftime("%H:%M:%S", time.localtime(self.timestamp))
+        return self.type + " event " + self.name
 
     def __str__(self):
         return self.to_string()
@@ -30,26 +28,34 @@ class Event:
     def __repr__(self):
         return self.to_string()
 
+
 class FEL:
-    def schedule(self, event, priority):
+    def schedule(self, event, timestamp):
         """
-        Schedules an event into the priority queue.
+        Schedules an event into the FEL.
         :param event: the event to schedule
-        :param priority: the priority of the event to schedule
+        :param timestamp: the timestamp of the event to schedule
         """
-        self.priority_queue.append((event, priority))
+        self.priority_queue.append((event, timestamp))
         self.priority_queue.sort(key=lambda pair: pair[1])
 
     def remove(self):
         """
-        Removes and returns the item at the front of the priority queue
-        :return: the item at the front of the priority queue
+        Removes and returns the item at the front of the FEL
+        :return: the item at the front of the FEL
         """
         return self.priority_queue.pop(0)
 
+    def peek(self):
+        """
+        Returns the item at the front of the FEL without removing it.
+        :return: the item at the front of the FEL
+        """
+        return self.priority_queue[0]
+
     def delete(self, event):
         """
-        Removes and returns a specified Event object from the priority queue
+        Removes and returns a specified Event object from the FEL
         :param event: the Event object to remove
         :return: the removed Event object
         """
@@ -63,12 +69,13 @@ class FEL:
 
     def __init__(self):
         self.priority_queue = []
+        self.now = 0
 
     def to_string(self):
         string = "["
 
-        for event, priority in self.priority_queue:
-            string += "{ " + str(event) + ", " + str(priority) + " },"
+        for event, timestamp in self.priority_queue:
+            string += "{" + str(event) + ", " + str(timestamp) + "},"
 
         string += "]"
 
@@ -87,5 +94,6 @@ def run_sim(fel):
     :param fel: the FEL to execute
     """
     while not fel.is_empty():
-        event, priority = fel.remove()
+        event, timestamp = fel.remove()
         event.handle()
+        fel.now = timestamp
